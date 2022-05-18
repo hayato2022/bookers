d749ace0d63b7cfd2aab9a1ccec8f7d89ef5ff4d
hayato2022 <gaomusunxiang@gmail.com>
2022-05-18 09:18:20 +0000

    [Add]バリデーション、フラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    book=Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book=Book.new(book_params)
+
+    if @book.save
+    redirect_to book_path(@book.id),notice: 'Book was successfully created.'
+    else
+    @books=Book.all
+    render :index
+    end
+
   end
 
   def index
@@ -24,15 +30,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book=Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book[:id])
+    @book=Book.find(params[:id])
+   if @book.update(book_params)
+    redirect_to book_path(@book[:id]), notice: 'Book was successfully updated.'
+   else
+    render :edit
+   end
   end
 
   def destroy
-    book=Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    @book=Book.find(params[:id])
+    if @book.destroy
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
+    end
   end
 private
  def book_params
